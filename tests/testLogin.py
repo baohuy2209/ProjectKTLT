1c24dc552d0b5547303b0479bdedc216233929fc
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2025-01-22 14:58:28 +0700

    Update Login

---
+++ tests/testLogin.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from backend.LoginWindowExt import LoginWindowExt
+
+app=QApplication([])
+myWindow=LoginWindowExt()
+myWindow.setupUi(QMainWindow())
+myWindow.show()
+app.exec()
