d50eeacd03712eb056dd010970da09f6e642d557
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2025-01-22 14:57:46 +0700

    change something

--- backend/TestWindowExt.py
+++
@@ -1,9 +0,0 @@
-from ui.TestWindow import Ui_MainWindow
-
-
-class TestWindowExt(Ui_MainWindow):
-    def setupUi(self, MainWindow):
-        super().setupUi(MainWindow)
-        self.MainWindow = MainWindow
-    def show(self):
-        self.MainWindow.show()
