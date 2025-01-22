153465a6f812ff8feea7e3b7db2fdf494da13a4a
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2025-01-22 14:20:48 +0700

    tempurature

---
+++ backend/LoginWindowExt.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QMainWindow
+
+from ui.LoginWindow import Ui_MainWindow
+
+
+class LoginWindowExt(Ui_MainWindow):
+    def __init__(self):
+        pass
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
+        self.MainWindow=MainWindow
+    def show(self):
+        self.MainWindow.show()
+
+
--- tests/testLogin.py
+++ tests/testLogin.py
@@ -1,6 +1,10 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
+<<<<<<< Updated upstream
 from backend.TestWindowExt import TestWindowExt
+=======
+from backend.LoginWindowExt import LoginWindowExt
+>>>>>>> Stashed changes
 
 app = QApplication([])
 myWindow = TestWindowExt()
